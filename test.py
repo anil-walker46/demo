7cf241aa5334bc889d13c71b02a42b36c4affa4a
anil-walker46 <110482662+anil-walker46@users.noreply.github.com>
2022-08-03 13:43:24 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,30 @@
+import time
+start = time.time()
+class queue:
+    def __init__(self):
+        self.items = []
+    def isEmpty(self):
+        return self.items ==[]
+    def enqueue(self.items):
+        self.item.append(item)
+        print(item)
+    def dequeu(self):
+        return self.items.pop(0)
+    def front(self):
+        return self.items[len(self.items)-1]
+    def size(self):
+        return len(self.items)
+q=Queue()
+print(q.isEmpty(),"- because queue is empty")
+print("Enquee")
+q.enqueue(15)
+q.enqueue(16)
+q.enqueue(17)
+print("Front",q.front())
+print("Dequee")
+print(q.dequeue())
+print(q.dequeue())
+print("Size",q.size())
+end = time.time()
+print(f"Runtime of the program is {end - start}")
+    
